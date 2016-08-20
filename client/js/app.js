2124ee1e4a35f7438a4e70fc398d3925d3ce4b1a
Luciano de Oliveira Júnior <luciannojunior@gmail.com>
2016-08-20 15:07:52 -0300

    api

--- client/js/app.js
+++ client/js/app.js
@@ -5,11 +5,10 @@ app.controller('ConvenioCidadeController', ['$scope', function($scope){
 
     $scope.convenios = [];
 
-    $scope.convenioSelecionado = {};
-
     var coisas = 
 [{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"},{"regCge":"16-80811-8","numero":"0331/2016","orgao":"SEE - 22.0001 - SECRETARIA DE ESTADO DA EDUCAÇÃO","convenente":"FUNDAÇÃO DE APOIO A PESQUISA DO ESTADO DA PARAÍBA","dataCelebracao":"1/8/2016","dataPublicacao":"4/8/2016","objetivo":"DESENVOLVER KITS DE ENSINO E APLICAÇÃO A SEREM UTILIZADOS EM ESCOLAS DE ENSINO MÉDIO DO ESTADO DA PARAÍBA","valorTotal":"18.000"}];
 
+    $scope.convenioSelecionado = coisas[0];
 
     $scope.envia = function(busca){
         $scope.temTabela = true;
@@ -18,9 +17,13 @@ app.controller('ConvenioCidadeController', ['$scope', function($scope){
 
     $scope.especifica = function(id){
         $scope.temTabela = false;
-        alert(id);
+        $scope.temDetalhe = true;
     };
 
+    $scope.voltaLista = function(){
+        $scope.temTabela = true;
+        $scope.temDetalhe = false;
+    };
 
 
 }]);
