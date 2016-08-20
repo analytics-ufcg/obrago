a48d2c07311b7e7cfdeb0fa22805527928cff2c8
Diego Coelho <diegocoelho.13@gmail.com>
2016-08-20 14:51:25 -0300

    comprimi que tava tenso

--- R/limpa_federais.R
+++ R/limpa_federais.R
@@ -1,5 +1,6 @@
 #install.packages("readr")
 require(readr)
-federal <- read_csv(file = "tudo-pronto.csv")
-table(pb$MUNIC_PROPONENTE)
-q
+federal <- read_csv(file = "../data/convenios_federais.csv")
+interessantes_cg <- federal[grepl("construção|reforma|construir|rua",federal$OBJETO_PROPOSTA, ignore.case = T) & federal$MUNIC_PROPONENTE=="CAMPINA GRANDE",]
+interessantes_cg <- interessantes_cg[,c("SIT_CONVENIO","NR_CONVENIO","DIA_INIC_VIGENC_CONV","DIA_FIM_VIGENC_CONV","DIA_LIMITE_PREST_CONTAS","ID_PROPOSTA","VL_GLOBAL_CONV","VL_REPASSE_CONV","VL_CONTRAPARTIDA_CONV","VL_EMPENHADO_CONV","VL_DESEMBOLSADO_CONV","VL_SALDO_REMAN_TESOURO","VL_INGRESSO_CONTRAPARTIDA","UF_PROPONENTE","MUNIC_PROPONENTE","OBJETO_PROPOSTA","funcao.imputada","TX_STATUS")]
+write.table(interessantes_cg,file = "../data/interessantes_cg_fed.csv", sep = "|", row.names = F)
